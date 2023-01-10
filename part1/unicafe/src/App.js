96f44b226b8ceb31a3cfe7dec61f9f351b9291f2
Juho Niinikoski <juho.niinikoski@aalto.fi>
2023-01-10 14:15:15 +0200

    1.7 done

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,6 +6,10 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const all = good + neutral + bad
+  const average = (good - bad) / all
+  const positive = (good / all) * 100
+
   return (
     <div>
       <h2>Give feedback</h2>
@@ -16,6 +20,9 @@ const App = () => {
       <p>Good {good}</p>
       <p>Neutral {neutral}</p>
       <p>Bad {bad}</p>
+      <p>All {all}</p>
+      <p>Average {average}</p>
+      <p>Positive {positive} %</p>
     </div>
   )
 }
