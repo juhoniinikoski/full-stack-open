f2884c3105593f2f4a9c076a850076ad5be2bdf5
Juho Niinikoski <juho.niinikoski@aalto.fi>
2023-01-10 14:04:05 +0200

    1.5 done

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -8,27 +8,29 @@ const Content = ({parts}) => parts.map(p => <Part key={p.name} part={p}/>)
 const Total = ({parts}) => <p>Number of exercises {parts.map(p => p.exercises).reduce((a, b) => a + b)}</p>
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course}/>
-      <Content parts={parts}  />
-      <Total parts={parts}/>
+      <Header course={course.name}/>
+      <Content parts={course.parts}  />
+      <Total parts={course.parts}/>
     </div>
   )
 }
