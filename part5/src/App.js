3061d95a7ed45256af03fffc6466ad2c13dce5b0
Juho Niinikoski <juho.niinikoski@aalto.fi>
2023-01-20 14:08:21 +0200

    5.5 - 5.6 done

--- part5/src/App.js
+++ part5/src/App.js
@@ -1,20 +1,19 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import Blog from './components/Blog'
+import BlogForm from './components/BlogForm'
+import LoginForm from './components/LoginForm'
+import Togglable from './components/Togglable'
 import { setToken, getAll, create } from './services/blogs'
 import { login } from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
-  const [username, setUsername] = useState('') 
-  const [password, setPassword] = useState('')
-  const [title, setTitle] = useState('')
-  const [author, setAuthor] = useState('')
-  const [url, setUrl] = useState('')
   const [message, setMessage] = useState('')
   const [user, setUser] = useState(null)
 
-  const handleLogin = async (event) => {
-    event.preventDefault()
+  const formRef = useRef()
+
+  const handleLogin = async (username, password) => {
     
     try {
       const user = await login({
@@ -25,8 +24,6 @@ const App = () => {
       )
       setToken(user.token)
       setUser(user)
-      setUsername('')
-      setPassword('')
     } catch (exception) {
       setMessage('wrong credentials')
       setTimeout(() => {
@@ -41,21 +38,13 @@ const App = () => {
     window.localStorage.removeItem('loggedUser')
   }
 
-  const handleCreate = async event => {
-    event.preventDefault()
+  const handleCreate = async newBlog => {
 
     try {
-      const newBlog = {
-        title: title,
-        author: author,
-        url: url
-      }
 
       const response = await create(newBlog)
       setBlogs([...blogs, response])
-      setTitle('')
-      setAuthor('')
-      setUrl('')
+
       setMessage(`a new blog ${response.title} by ${response.author} added`)
       setTimeout(() => {
         setMessage(null)
@@ -84,28 +73,10 @@ const App = () => {
   if (!user) {
     return (
       <div>
-        <h2>Login</h2>
-        <form onSubmit={handleLogin}>
-          <div>
-            username
-              <input
-              type="text"
-              value={username}
-              name="Username"
-              onChange={({ target }) => setUsername(target.value)}
-            />
-          </div>
-          <div>
-            password
-              <input
-              type="password"
-              value={password}
-              name="Password"
-              onChange={({ target }) => setPassword(target.value)}
-            />
-          </div>
-          <button type="submit">login</button>
-        </form>
+        {message && <p>{message}</p>}
+        <LoginForm
+          handleLogin={handleLogin}
+        />
       </div>
     )
   }
@@ -115,37 +86,9 @@ const App = () => {
       {message && <p>{message}</p>}
       <h2>blogs</h2>
       <p>{user.name} logged in <button onClick={handleLogout} type='button'>log out</button></p>
-      <h2>create new</h2>
-      <form onSubmit={handleCreate}>
-        <div>
-          title
-            <input
-            type="text"
-            value={title}
-            name="Title"
-            onChange={({ target }) => setTitle(target.value)}
-          />
-        </div>
-        <div>
-          author
-            <input
-            type="text"
-            value={author}
-            name="Author"
-            onChange={({ target }) => setAuthor(target.value)}
-          />
-        </div>
-        <div>
-          url
-            <input
-            type="text"
-            value={url}
-            name="Url"
-            onChange={({ target }) => setUrl(target.value)}
-          />
-        </div>
-        <button type="submit">create</button>
-      </form>
+      <Togglable buttonLabel='new blog' ref={formRef}>
+        <BlogForm handleCreate={handleCreate} />
+      </Togglable>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
